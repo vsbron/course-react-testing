bf99712222ce8b5128aa56f527053c09ff6a1aaf
Борис Макаров <vsbron@gmail.com>
2025-10-02 14:01:06 +0500

    01-rtl-tutorial - Set Sandbox 01 component

--- 01-rtl-tutorial/src/tutorial/01-search-by-text/Sandbox.tsx
+++ 01-rtl-tutorial/src/tutorial/01-search-by-text/Sandbox.tsx
@@ -1,4 +1,33 @@
+import { useEffect, useState } from "react";
+
 const Sandbox = () => {
-  return <div>Sandbox</div>;
+  // Create state values for message and error
+  const [showMessage, setShowMessage] = useState<boolean>(false);
+  const [showError, setShowError] = useState<boolean>(false);
+
+  // useEffect function that changes showMessage state
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowMessage(true);
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>React Testing Library examples</h1>
+      <p>You can search me with regular expression: 123-456-7890</p>
+      {showError && <p>Error message</p>}
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+      </ul>
+      {showMessage && <p>Async message</p>}
+    </div>
+  );
 };
 export default Sandbox;
