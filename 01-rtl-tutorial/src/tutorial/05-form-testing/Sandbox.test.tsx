294221cd58405a84a54bc0971bcfe49ec7c3d263
Борис Макаров <vsbron@gmail.com>
2025-10-03 14:22:39 +0500

    01-rtl-tutorial - 05 - Add tests for typing values and checking them

--- 01-rtl-tutorial/src/tutorial/05-form-testing/Sandbox.test.tsx
+++ 01-rtl-tutorial/src/tutorial/05-form-testing/Sandbox.test.tsx
@@ -21,4 +21,25 @@ describe("05-form-testing", () => {
     const confirmPasswordInput = screen.getByLabelText("Confirm password:");
     expect(confirmPasswordInput).toHaveValue("");
   });
+
+  test("should be able to type in the input", async () => {
+    render(<Sandbox />);
+    // Get the user
+    const user = userEvent.setup();
+
+    // Get the input fields
+    const emailInputElement = screen.getByRole("textbox", { name: /email/i });
+    const passwordInput = screen.getByLabelText("Password:");
+    const confirmPasswordInput = screen.getByLabelText("Confirm password:");
+
+    // Test the typings
+    await user.type(emailInputElement, "test@test.com");
+    await user.type(passwordInput, "123456");
+    await user.type(confirmPasswordInput, "123456");
+
+    // Test the values
+    expect(emailInputElement).toHaveValue("test@test.com");
+    expect(passwordInput).toHaveValue("123456");
+    expect(confirmPasswordInput).toHaveValue("123456");
+  });
 });
