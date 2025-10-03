68961acae2c2cd490b0d06e800037aaccd352799
Борис Макаров <vsbron@gmail.com>
2025-10-03 14:05:16 +0500

    01-rtl-tutorial - 05 - Set up the JSX for testing

--- 01-rtl-tutorial/src/App.tsx
+++ 01-rtl-tutorial/src/App.tsx
@@ -1,13 +1,8 @@
-import Sandbox from "./tutorial/04-user-interactions/Sandbox";
+import Sandbox from "./final/05-form-testing/Sandbox";
 
 function App() {
   return (
     <div className="p-8">
-      <h1 className="font-bold text-2xl">React Testing Library</h1>
-      <p className="mt-4 text-gray-700">
-        React Testing Library and Vitest work together to provide a robust
-        testing environment.
-      </p>
       <Sandbox />
     </div>
   );
--- 01-rtl-tutorial/src/tutorial/05-form-testing/Sandbox.tsx
+++ 01-rtl-tutorial/src/tutorial/05-form-testing/Sandbox.tsx
@@ -1,4 +1,91 @@
+import { useState } from "react";
+import validator from "validator";
+
+const labelStyles = "block text-grey-700 font-medium mb-2";
+const inputStyles = "w-full px-3 py-2 border border-gray-300 rounded-md";
+const buttonStyles =
+  "w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600";
+const defaultFormState = {
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
 const Sandbox = () => {
-  return <div>Sandbox</div>;
+  // Create state values for inputs and error
+  const [signUpInput, setSignUpInput] = useState(defaultFormState);
+  const [error, setError] = useState<string>("");
+
+  // Create handler functions
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setSignUpInput((prevInputs) => ({ ...prevInputs, [id]: value }));
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
+    if (!validator.isEmail(signUpInput.email)) {
+      return setError("Invalid email");
+    }
+    if (!validator.isLength(signUpInput.password, { min: 5 })) {
+      return setError("Password must be at least 5 characters");
+    }
+    if (signUpInput.confirmPassword !== signUpInput.password) {
+      return setError("Passwords do not match");
+    }
+    setError("");
+    setSignUpInput(defaultFormState);
+  };
+
+  // Returned JSX
+  return (
+    <div className="container mx-auto max-w-md mt-10 p-6 bg-white rounded-lg shadow-mg">
+      <form className="space-y-4">
+        <div className="mb-3">
+          {/* Email input */}
+          <label htmlFor="email" className={labelStyles}>
+            Email address:
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={signUpInput.email}
+            onChange={handleChange}
+            className={inputStyles}
+          />
+
+          {/* Password input */}
+          <label htmlFor="password" className={labelStyles}>
+            Password:
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={signUpInput.password}
+            onChange={handleChange}
+            className={inputStyles}
+          />
+
+          {/* Confirm password input */}
+          <label htmlFor="confirmPassword" className={labelStyles}>
+            Confirm password:
+          </label>
+          <input
+            type="password"
+            id="confirmPassword"
+            value={signUpInput.confirmPassword}
+            onChange={handleChange}
+            className={inputStyles}
+          />
+        </div>
+
+        {/* Error message */}
+        {error && <p className="text-red-500 text-sm">{error}</p>}
+
+        {/* Submit button */}
+        <button type="button" onClick={handleSubmit} className={buttonStyles}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 export default Sandbox;
