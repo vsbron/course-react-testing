b2c9d51561f26a86bd320c3141edf41b28389f3e
Борис Макаров <vsbron@gmail.com>
2025-10-04 11:57:49 +0500

    01-rtl-tutorial - 05 - Refactor the Tests code

--- 01-rtl-tutorial/src/tutorial/05-form-testing/Sandbox.test.tsx
+++ 01-rtl-tutorial/src/tutorial/05-form-testing/Sandbox.test.tsx
@@ -1,45 +1,60 @@
-import { render, screen, logRoles, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent, { type UserEvent } from "@testing-library/user-event";
 import Sandbox from "./Sandbox";
 
+// Set the form elements selector
+const getFormElements = () => {
+  const elements = {
+    emailInput: screen.getByRole("textbox", { name: /email/i }),
+    passwordInput: screen.getByLabelText("Password:"),
+    confirmPasswordInput: screen.getByLabelText("Confirm password:"),
+    submitButton: screen.getByRole("button", { name: /submit/i }),
+  };
+  return elements;
+};
+
 describe("05-form-testing", () => {
-  test("inputs should be initially empty", () => {
+  let user: UserEvent;
+
+  // Do before each test
+  beforeEach(() => {
+    // Get the user (must come first)
+    user = userEvent.setup();
+
     // Render the sandbox
-    const { container } = render(<Sandbox />);
-    logRoles(container); //Log the roles
+    render(<Sandbox />);
+  });
+
+  // TEST #1
+  test("inputs should be initially empty", () => {
+    // Get the input elements
+    const { emailInput, passwordInput, confirmPasswordInput } =
+      getFormElements();
 
     // Debug the screen
     screen.debug();
 
-    // Get the email input field
-    const emailInputElement = screen.getByRole("textbox", { name: /email/i });
-    expect(emailInputElement).toHaveValue("");
-
-    // Get the passwords input field
-    const passwordInput = screen.getByLabelText("Password:");
+    // Get the inputs` field
+    expect(emailInput).toHaveValue("");
     expect(passwordInput).toHaveValue("");
-    const confirmPasswordInput = screen.getByLabelText("Confirm password:");
     expect(confirmPasswordInput).toHaveValue("");
   });
 
+  // TEST #2
   test("should be able to type in the input", async () => {
-    render(<Sandbox />);
-    // Get the user
-    const user = userEvent.setup();
-
-    // Get the input fields
-    const emailInputElement = screen.getByRole("textbox", { name: /email/i });
-    const passwordInput = screen.getByLabelText("Password:");
-    const confirmPasswordInput = screen.getByLabelText("Confirm password:");
+    // Get the input elements
+    const { emailInput, passwordInput, confirmPasswordInput } =
+      getFormElements();
 
     // Test the typings
-    await user.type(emailInputElement, "test@test.com");
+    await user.type(emailInput, "test@test.com");
     await user.type(passwordInput, "123456");
     await user.type(confirmPasswordInput, "123456");
 
     // Test the values
-    expect(emailInputElement).toHaveValue("test@test.com");
+    expect(emailInput).toHaveValue("test@test.com");
     expect(passwordInput).toHaveValue("123456");
     expect(confirmPasswordInput).toHaveValue("123456");
   });
+  ``;
 });
